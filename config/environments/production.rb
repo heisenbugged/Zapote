334769f737042318a3c33db4ad3a7b6c564a61a9
James Strong <jstrong@james-mac.gateway.2wire.net>
2011-04-26 14:08:07 -0500

    tst

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,4 +46,16 @@ EnglishQuiz::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+
+
+  CarrierWave.configure do |config|
+    config.storage = :fog
+    config.fog_credentials = {
+      :provider               => 'AWS',
+      :aws_access_key_id      => 'AKIAJ6L6AVJBLUDZNQXQ',
+      :aws_secret_access_key  => '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'
+    }
+    config.fog_directory  = 'etherpros'
+  end
+
 end
