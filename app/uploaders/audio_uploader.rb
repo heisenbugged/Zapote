11076be796ef0a84fc19466916f63f6540723703
James Strong <jstrong@james-mac.gateway.2wire.net>
2011-04-26 14:01:10 -0500

    fog fix test heroku

--- app/uploaders/audio_uploader.rb
+++ app/uploaders/audio_uploader.rb
@@ -10,12 +10,6 @@ class AudioUploader < CarrierWave::Uploader::Base
   storage :fog
   # storage :s3
 
-  # Override the directory where uploaded files will be stored.
-  # This is a sensible default for uploaders that are meant to be mounted:
-  def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  end
-
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
